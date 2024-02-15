3adc554516f2aa5186ffa7e9c079023465485231
João Conte <lejoaoconte@gmail.com>
2024-02-15 14:32:07 -0300

    Dinamic Lists

---
+++ List/DinamicList/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+void main()
+{
+  Car cars[6] = {
+      {"Car 1", "Brand A", 10000.50},
+      {"Car 2", "Brand B", 15000.75},
+      {"Car 3", "Brand C", 20000.00},
+      {"Car 4", "Brand D", 25000.25},
+      {"Car 5", "Brand E", 30000.50},
+      {"Car 6", "Brand F", 35000.75}};
+
+  return;
+}
